93fc348c72e7c2cd09f335035fa10944b1c7353a
Pawan Kumar Gupta <133222848+Pawan-079@users.noreply.github.com>
2024-11-21 21:20:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express=require("express");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
+
 const app=express();
 
 app.use("/api/v1/user",userRouter);
 app.use("api/v1/course",courseRouter);
+app.use("api/v1/admin",adminRouter);
 
 
-createUserRouts(express);
-createCourseRouts(express);
+// createUserRouts(express);
+// createCourseRouts(express);
 
 express.listen(3000); 
